aff77b9576734adc23ec02f0727a3fd0a15ae0c3
unknown <estin1996@gmail.com>
2020-11-13 00:39:52 +0200

    updated navbar and searchbar UI

--- header.php
+++ header.php
@@ -15,20 +15,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/39ba5b7d11.js" crossorigin="anonymous"></script>
     <title>Social Network</title>
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="/">
-        <img src="/Images/logoMinimal.svg" width="35" height="35" alt="" loading="lazy" class="mt-sm-n2 px-sm-1 border border-dark rounded-circle">
+        <img src="/Images/logoMinimal.svg" width="38" height="38" alt="" loading="lazy" class="px-sm-1 border border-dark rounded-circle">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <form class="form-inline my-2 my-lg-0">
+            <div class="ml-sm-n2 border border-primary rounded-pill" style="position:relative; width:272px; height:40px;">
+                <i class="fas fa-search fa-lg" style="position:absolute; left:11px;top:13px; color:#99abb6;"></i>
+                <input class="form-control border-white rounded-pill" style="width:230px; position:absolute; left:40px;" type="search" placeholder="Search" aria-label="Search">
+            </div>
+            <button class="btn btn-outline-success my-2 my-sm-0 d-none" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
@@ -50,9 +58,5 @@
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
     </div>
 </nav>
