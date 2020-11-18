878a4f35f28c22232f846e8db285477368f29418
unknown <estin1996@gmail.com>
2020-11-18 22:23:20 +0200

    added notification and chat button on navbar

--- header.php
+++ header.php
@@ -38,12 +38,9 @@
             <button class="btn btn-outline-success my-2 my-sm-0 d-none" type="submit">Search</button>
         </form>
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
+            <li class="nav-item dropdown mr-sm-2">
+                <a class="nav-link dropdown-toggle dropdown-toggle-estin btn btn-estin rounded-circle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-comment fa-lg" style="color:white;"></i>
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Action</a>
@@ -52,13 +49,24 @@
                     <a class="dropdown-item" href="#">Something else here</a>
                 </div>
             </li>
-            <li class="nav-item">
+            <li class="nav-item dropdown mr-sm-2">
+                <a class="nav-link dropdown-toggle dropdown-toggle-estin btn btn-estin rounded-circle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-bell fa-lg" style="color:white;"></i>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item mr-sm-2">
                 <a class="nav-link btn btn-estin rounded-circle" href="/Includes/logout.inc.php">
-                    <i class="fas fa-sign-out-alt fa-2x" style="color:white;"></i>
+                    <i class="fas fa-sign-out-alt fa-lg" style="color:white;"></i>
                 </a>
             </li>
         </ul>
     </div>
 </nav>
 
-<div style="height:60px" id="navbarOffset"></div>
+<div style="height:56px" id="navbarOffset"></div>
